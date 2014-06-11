Fixing for units and wavelength<->wavenumber conversions

pyspectral/near_infrared_reflectance.py
@@ -121,9 +121,10 @@ class Calculator(RadTbConverter):
             
         if not self.rsr:
             # What should this really be?
-            # ch37name or ch11name!!??
+            # ch37name or ch11name!?
+            # Check doc!
             # FIXME!
-            thermal_emiss_one = self.tb2radiance_simple(tb_therm, ch11name)
+            thermal_emiss_one = self.tb2radiance_simple(tb_therm, ch37name)
             l_nir = self.tb2radiance_simple(tb_nir, ch37name)
         else:
             thermal_emiss_one = self.tb2radiance(tb_therm, ch37name)
@@ -131,7 +132,6 @@ class Calculator(RadTbConverter):
 
         LOG.info('thermal_emiss_one = ' + str(thermal_emiss_one))
         LOG.info('l_nir = ' + str(l_nir))
-
 
         mu0 = np.cos(np.deg2rad(sunz))
         #mu0 = np.where(np.less(mu0, 0.1), 0.1, mu0)

pyspectral/radiance_tb_conversion.py
@@ -114,6 +114,10 @@ class RadTbConverter(object):
         else:
             self.wavespace = WAVE_LENGTH
 
+        self._wave_unit = ''
+        self._wave_si_scale = 1.0
+
+
         if 'tb_resolution' in options:
             self.tb_resolution = options['tb_resolution']
         else:
@@ -126,56 +130,21 @@ class RadTbConverter(object):
     def get_rsr(self):
         """Get all spectral responses for the sensor"""
 
+        from utils import convert2wavenumber
         from pyspectral.rsr_reader import RelativeSpectralResponse
+
         sensor = RelativeSpectralResponse(self.platform, self.satnumber, 
                                           self.instrument)
-        self.rsr = sensor.rsr
+        LOG.debug("Wavenumber? " + str(self.wavespace))
+        if self.wavespace == WAVE_NUMBER:
+            LOG.debug("Converting to wavenumber...")
+            self.rsr, info = convert2wavenumber(sensor.rsr)
+        else:
+            self.rsr = sensor.rsr
+            info = {'unit': sensor.unit, 'si_scale': sensor.si_scale}
 
-    # def get_rsr(self, channels=None):
-    #     """Read in all relative spectral responses for the sensor"""
-
-    #     from scipy.interpolate import InterpolatedUnivariateSpline
-    #     from pyspectral.rsr_reader import RelativeSpectralResponse
-
-    #     satname = self._getsatname()
-    #     seviri = Seviri()
-    #     self.rsr = {}
-    #     for key in seviri.rsr.keys():
-    #         if channels and key not in channels:
-    #             # Load only the requested channels/bands 
-    #             continue
-
-    #         wvl = seviri.rsr[key]['wavelength']
-    #         try: 
-    #             resp = seviri.rsr[key][satname]['95']
-    #         except ValueError:
-    #             resp = seviri.rsr[key][satname]
-
-    #         # Conversion from wavelengths to wave numbers:
-    #         if self.wavespace == WAVE_NUMBER:
-    #             wvl = 1./wvl[::-1] 
-    #             resp = resp[::-1]
-    #             scale = 1000000.0
-    #             unit = 'm-1'
-    #         elif self.wavespace == WAVE_LENGTH:
-    #             scale = 1./1000000.0 # microns -> meters
-    #             unit = 'm'
-    #         else:
-    #             raise AttributeError('wavespace ' + self.wavespace + 
-    #                                  ' not supported!')
-
-    #         wvl = wvl * scale
-    #         start = wvl[0]
-    #         end = wvl[-1]
-    #         dlambda = (end - start) / 3000.
-            
-    #         xspl = np.linspace(start, end, (end-start)/dlambda)
-    #         ius = InterpolatedUnivariateSpline(wvl, resp)
-    #         resp_ipol = ius(xspl)
-    #         wavel = xspl
-
-    #         self.rsr[key] = {self.wavespace: wavel, 'response': resp_ipol, 
-    #                          'units': (unit, None)}
+        self._wave_unit = info['unit']
+        self._wave_si_scale = info['si_scale']
 
     def _getsatname(self):
         """Get the satellite name used in the rsr-reader, from the platform and
@@ -202,15 +171,22 @@ class RadTbConverter(object):
             start = int(lut['tb'][0] * self.tb_scale)
             return lut['radiance'][ntb - start]
 
-        if not self.wavespace == WAVE_LENGTH:
-            raise NotImplementedError('Wavenumber representation of ' + 
-                                      'rsr data not implemented at the moment!')
+        if self.wavespace == WAVE_LENGTH:
+            wv_ = (self.rsr[bandname][self.detector]['wavelength'] * 
+                   self._wave_si_scale)
+            resp = self.rsr[bandname][self.detector]['response']
+            planck = blackbody(wv_, tb_) * resp
+        elif self.wavespace == WAVE_NUMBER:
+            wv_ = (self.rsr[bandname][self.detector]['wavenumber'] * 
+                   self._wave_si_scale)
+            resp = self.rsr[bandname][self.detector]['response']
+            planck = blackbody_wn(wv_, tb_) * resp
+        else:
+            raise NotImplementedError(str(self.wavespace) + 
+                                      ' representation of ' + 
+                                      'rsr data not supported!')
 
-        wvl = self.rsr[bandname][self.detector]['wavelength']
-        resp = self.rsr[bandname][self.detector]['response']
-        planck = blackbody(wvl, tb_) * resp
-
-        return integrate.trapz(planck, wvl)
+        return integrate.trapz(planck, wv_)
 
     def make_tb2rad_lut(self, bandname, filepath):
         """Generate a Tb to radiance look-up table"""
@@ -225,7 +201,6 @@ class RadTbConverter(object):
 
         retv = np.load(filepath, 'r')
         return retv
-
 
     def tb2radiance_simple(self, tb_, bandname):
         """Get the radiance from the Tb using the simple non-linear regression

pyspectral/tests/test_reflectance.py
@@ -23,8 +23,6 @@
 """Unit testing the 3.7 micron reflectance calculations
 """
 
-from mock import MagicMock
-
 from pyspectral.near_infrared_reflectance import Calculator
 
 import unittest
@@ -40,7 +38,7 @@ TEST_RSR['20']['det-1']['wavelength'] = np.array([
         3.8063589,  3.8163606,  3.8264089,  3.8364836,  3.8463381,
         3.8563975,  3.8664163,  3.8763755,  3.8864797,  3.8964978,
         3.9064275,  3.9164873,  3.9264729,  3.9364026,  3.9465107,
-        3.9535347], dtype='double') * 1e-6
+        3.9535347], dtype='double')
 
 TEST_RSR['20']['det-1']['response'] = np.array([
         0.01   ,  0.0118 ,  0.01987,  0.03226,  0.05028,  0.0849 ,
@@ -53,8 +51,9 @@ TEST_RSR['20']['det-1']['response'] = np.array([
 class ProductionClass(Calculator):
     def get_rsr(self):
         self.rsr = TEST_RSR
-        print("Calling static reader")
-
+        self._wave_unit = '1e-6 m'
+        self._wave_si_scale = 1e-6
+        
 
 class TestReflectance(unittest.TestCase):
     """Unit testing the reflectance calculations"""
@@ -63,13 +62,13 @@ class TestReflectance(unittest.TestCase):
         """Set up"""
 
     def test_reflectance(self):
-        """Test the derivation of the refletive part of a 3.7 micron band"""
+        """Test the derivation of the reflective part of a 3.7 micron band"""
 
         refl37 = ProductionClass('eos', '2', 'modis', '20')
         
         SUNZ = 80.
-        TB3 = 290
-        TB4 = 282
+        TB3 = 290.
+        TB4 = 282.
         REFL = refl37.reflectance_from_tbs(SUNZ, TB3, TB4)
         self.assertAlmostEqual(REFL, 0.251245010648)
 