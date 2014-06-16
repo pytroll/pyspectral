Bug fixes and corrections to the reflectance calculations. Added units and scale.

pyspectral/blackbody.py
@@ -38,7 +38,7 @@ EPSILON = 0.000001
 
 
 def blackbody_wn(wavenumber, temp):
-    """The Planck radiation or Blackbody radiation as a function of wave number
+    """The Planck radiation or Blackbody radiation as a function of wavenumber
     SI units!
     blackbody_wn(wavnum, temperature)
     wavenumber = A wavenumber (scalar) or a sequence of wave numbers (m-1)
@@ -64,9 +64,9 @@ def blackbody_wn(wavenumber, temp):
     else:
         wavnum = np.array(wavenumber, dtype='float64')
 
-    print("mid wavenumber: " + str(wavnum[wavnum.shape[0] / 2]))
+    #print("mid wavenumber: " + str(wavnum[wavnum.shape[0] / 2]))
 
-    nom = 2 * H_PLANCK * C_SPEED * C_SPEED * wavnum ** 3
+    nom = 2 * H_PLANCK * (C_SPEED ** 2) * (wavnum ** 3)
     arg1 = H_PLANCK * C_SPEED * wavnum / K_BOLTZMANN
     arg2 = np.where(np.greater(np.abs(temperature), EPSILON),
                     np.array(1. / temperature), -9).reshape(-1, 1)
@@ -119,7 +119,7 @@ def blackbody(wavel, temp):
     else:
         wavelength = np.array(wavel, dtype='float64')
 
-    print("mid wavelength: " + str(wavelength[wavelength.shape[0] / 2]))
+    #print("mid wavelength: " + str(wavelength[wavelength.shape[0] / 2]))
 
     const = 2 * H_PLANCK * C_SPEED ** 2
     nom = const / wavelength ** 5

pyspectral/near_infrared_reflectance.py
@@ -75,6 +75,10 @@ class Calculator(RadTbConverter):
         else:
             self.detector = 'det-1'
 
+        resp = self.rsr[self.bandname][self.detector]['response']
+        wv_ = self.rsr[self.bandname][self.detector][self.wavespace]
+        self.rsr_integral = np.trapz(resp, wv_)
+
     def derive_rad39_corr(self, bt11, bt13, method='rosenfeld'):
         """Derive the 3.9 radiance correction factor to account for the
         attenuation of the emitted 3.9 radiance by CO2 absorption. Requires the
@@ -90,17 +94,20 @@ class Calculator(RadTbConverter):
     def _get_solarflux(self):
         """Derive the in-band solar flux from rsr over the Near IR band (3.7 or
         3.9 microns)"""
+        # print "Inside _get_solarflux...", self.wavespace
+
         solar_spectrum = SolarIrradianceSpectrum(TOTAL_IRRADIANCE_SPECTRUM_2000ASTM,
-                                                 dlambda=0.0005)
+                                                 dlambda=0.0005,
+                                                 wavespace=self.wavespace)
+        # print self.rsr[self.bandname]
         self.solar_flux = solar_spectrum.inband_solarflux(
             self.rsr[self.bandname])
 
-    def reflectance_from_tbs(self, sunz, tb_nir, tb_therm,
-                             lookuptable=None):
+    def reflectance_from_tbs(self, sunz, tb_nir, tb_therm):
         """
-        The relfectance calculated is without units and should be between 0 and 1.
+        The relfectance calculated is without units and should be between 0 and
+        1.
         """
-        import os.path
 
         if np.isscalar(tb_nir):
             tb_nir = np.array([tb_nir, ])
@@ -112,25 +119,32 @@ class Calculator(RadTbConverter):
                              str(tb_therm.shape) + ' and ' + str(tb_nir.shape))
 
         if self.instrument == 'seviri':
-            ch11name = 'IR10.8'
             ch37name = 'IR3.9'
         elif self.instrument == 'modis':
-            ch11name = '31'
             ch37name = '20'
         else:
             raise NotImplementedError('Not yet support for this ' +
                                       'instrument ' + str(self.instrument))
 
         if not self.rsr:
-            # What should this really be?
-            # ch37name or ch11name!?
-            # Check doc!
-            # FIXME!
-            thermal_emiss_one = self.tb2radiance_simple(tb_therm, ch37name)
-            l_nir = self.tb2radiance_simple(tb_nir, ch37name)
+            raise NotImplementedError("Reflectance calculations without " +
+                                      "rsr not yet supported!")
+            # retv = self.tb2radiance_simple(tb_therm, ch37name)
+            # print("tb2radiance_simple conversion: " + str(retv))
+            # thermal_emiss_one = retv['radiance']
+            # retv = self.tb2radiance_simple(tb_nir, ch37name)
+            # print("tb2radiance_simple conversion: " + str(retv))
+            # l_nir = retv['radiance']
         else:
-            thermal_emiss_one = self.tb2radiance(tb_therm, ch37name)
-            l_nir = self.tb2radiance(tb_nir, ch37name)
+            # Assume rsr in in microns!!!
+            # FIXME!
+            scale = self.rsr_integral * 1e-6
+            retv = self.tb2radiance(tb_therm, ch37name)
+            #print("tb2radiance conversion: " + str(retv))
+            thermal_emiss_one = retv['radiance'] * scale
+            retv = self.tb2radiance(tb_nir, ch37name)
+            #print("tb2radiance conversion: " + str(retv))
+            l_nir = retv['radiance'] * scale
 
         LOG.info('thermal_emiss_one = ' + str(thermal_emiss_one))
         LOG.info('l_nir = ' + str(l_nir))

pyspectral/radiance_tb_conversion.py
@@ -186,14 +186,24 @@ class RadTbConverter(object):
                                       ' representation of ' +
                                       'rsr data not supported!')
 
-        return integrate.trapz(planck, wv_) / np.trapz(resp, wv_)
+        radiance = integrate.trapz(planck, wv_) / np.trapz(resp, wv_)
+        if self.wavespace == WAVE_NUMBER:
+            unit = 'W/m^2 sr^-1 (m^-1)^-1'
+            scale = 1.0
+        else:
+            unit = 'W/m^2 sr^-1 m^-1'
+            scale = 1.0
+
+        return {'radiance': radiance,
+                'unit': unit,
+                'scale': scale}
 
     def make_tb2rad_lut(self, bandname, filepath):
         """Generate a Tb to radiance look-up table"""
 
         tb_ = np.arange(TB_MIN, TB_MAX, self.tb_resolution)
-        rad = self.tb2radiance(tb_, bandname)
-
+        retv = self.tb2radiance(tb_, bandname)
+        rad = retv['radiance']
         np.savez(filepath, tb=tb_, radiance=rad.compressed())
 
     def read_tb2rad_lut(self, filepath):
@@ -221,11 +231,22 @@ class RadTbConverter(object):
         alpha = SEVIRI[bandname][self.satnumber][1]
         beta = SEVIRI[bandname][self.satnumber][2]
 
-        return c_1 * vc_ ** 3 / (np.exp(c_2 * vc_ / (alpha * tb_ + beta)) - 1)
+        radiance = c_1 * vc_ ** 3 / \
+            (np.exp(c_2 * vc_ / (alpha * tb_ + beta)) - 1)
+
+        unit = 'W/m^2 sr^-1 (m^-1)^-1'
+        scale = 1.0
+        #unit = 'mW/m^2 sr^-1 (cm^-1)^-1'
+        #scale = 10.0
+        return {'radiance': radiance,
+                'unit': unit,
+                'scale': scale}
 
     def radiance2tb_simple(self, rad, bandname):
         """Get the Tb from the radiance using the simple non-linear regression
-        method. SI units of course!"""
+        method. 
+        rad: Radiance in units = 'mW/m^2 sr^-1 (cm^-1)^-1'
+        """
         #
         # Tb = C2 * νc/{α * log[C1*νc**3 / L + 1]} - β/α
         #
@@ -242,4 +263,7 @@ class RadTbConverter(object):
         alpha = SEVIRI[bandname][self.satnumber][1]
         beta = SEVIRI[bandname][self.satnumber][2]
 
-        return c_2 * vc_ / (alpha * np.log(c_1 * vc_ ** 3 / rad + 1)) - beta / alpha
+        tb_ = c_2 * vc_ / \
+            (alpha * np.log(c_1 * vc_ ** 3 / rad + 1)) - beta / alpha
+
+        return tb_

pyspectral/solar.py
@@ -85,7 +85,9 @@ class SolarIrradianceSpectrum(object):
                           'flux': 'W/m^2'}
 
     def convert2wavenumber(self):
-        """Convert from wavelengths to wavenumber. 
+        """
+        Convert from wavelengths to wavenumber. 
+
         Units:
           Wavelength: micro meters (1e-6 m)
           Wavenumber: cm-1
@@ -118,11 +120,11 @@ class SolarIrradianceSpectrum(object):
 
         return self._band_calculations(rsr, True, scale, **options)
 
-    def inband_solarirradiance(self, rsr, scale=1.0, **options):
-        """Derive the inband solar irradiance for a given instrument relative
-        spectral response valid for an earth-sun distance of one AU."""
+    # def inband_solarirradiance(self, rsr, scale=1.0, **options):
+    #     """Derive the inband solar irradiance for a given instrument relative
+    #     spectral response valid for an earth-sun distance of one AU."""
 
-        return self._band_calculations(rsr, False, scale, **options)
+    #     return self._band_calculations(rsr, False, scale, **options)
 
     def _band_calculations(self, rsr, flux, scale, **options):
         """Derive the inband solar flux or inband solar irradiance for a given
@@ -183,6 +185,7 @@ class SolarIrradianceSpectrum(object):
         if flux:
             return np.trapz(irr * resp_ipol, wvl)
         else:
+            # Divide by the equivalent band width:
             return np.trapz(irr * resp_ipol, wvl) / np.trapz(resp_ipol, wvl)
 
     def interpolate(self, **options):

pyspectral/tests/test_rad_tb_conversions.py
@@ -156,37 +156,39 @@ class TestRadTbConversions(unittest.TestCase):
 
         res = self.modis.tb2radiance(TEST_TBS, '20', lut=False)
         print res
-        self.assertTrue(np.allclose(TRUE_RADS, res))
+        self.assertTrue(np.allclose(TRUE_RADS, res['radiance']))
 
         res = self.modis.tb2radiance(237., '20', lut=False)
-        self.assertAlmostEqual(16570.592171157, res)
+        self.assertAlmostEqual(16570.592171157, res['radiance'])
 
         res = self.modis.tb2radiance(277., '20', lut=False)
-        self.assertAlmostEqual(167544.3823631, res)
+        self.assertAlmostEqual(167544.3823631, res['radiance'])
 
         res = self.modis.tb2radiance(1.1, '20', lut=False)
-        self.assertAlmostEqual(0.0, res)
+        self.assertAlmostEqual(0.0, res['radiance'])
 
         res = self.modis.tb2radiance(11.1, '20', lut=False)
-        self.assertAlmostEqual(0.0, res)
+        self.assertAlmostEqual(0.0, res['radiance'])
 
         res = self.modis.tb2radiance(100.1, '20', lut=False)
-        self.assertAlmostEqual(5.3940515573e-06, res)
+        self.assertAlmostEqual(5.3940515573e-06, res['radiance'])
 
         res = self.modis.tb2radiance(200.1, '20', lut=False)
-        self.assertAlmostEqual(865.09776189, res)
+        self.assertAlmostEqual(865.09776189, res['radiance'])
 
     def test_conversion_simple(self):
         """Test the tb2radiance_simple function to convert radiances to Tb's"""
 
-        rads = self.sev2.tb2radiance_simple(TEST_TBS, 'IR3.9')
+        retv = self.sev2.tb2radiance_simple(TEST_TBS, 'IR3.9')
+        rads = retv['radiance']
         # Units space = wavenumber (cm-1):
         tbs = self.sev2.radiance2tb_simple(rads, 'IR3.9')
         self.assertTrue(np.allclose(TEST_TBS, tbs))
 
         np.random.seed()
         tbs1 = 200.0 + np.random.random(50) * 150.0
-        rads = self.sev2.tb2radiance_simple(tbs1, 'IR3.9')
+        retv = self.sev2.tb2radiance_simple(tbs1, 'IR3.9')
+        rads = retv['radiance']
         tbs = self.sev2.radiance2tb_simple(rads, 'IR3.9')
         self.assertTrue(np.allclose(tbs1, tbs))
 

pyspectral/tests/test_reflectance.py
@@ -31,47 +31,117 @@ import numpy as np
 TEST_RSR = {'20': {}}
 TEST_RSR['20']['det-1'] = {}
 TEST_RSR['20']['det-1']['wavelength'] = np.array([
-        3.6123999,  3.6163599,  3.6264927,  3.6363862,  3.646468 ,
-        3.6564937,  3.6664478,  3.6765388,  3.6865413,  3.6964585,
-        3.7065142,  3.716509 ,  3.7264658,  3.7364102,  3.7463682,
-        3.7563652,  3.7664226,  3.7763396,  3.7863384,  3.7964207,
-        3.8063589,  3.8163606,  3.8264089,  3.8364836,  3.8463381,
-        3.8563975,  3.8664163,  3.8763755,  3.8864797,  3.8964978,
-        3.9064275,  3.9164873,  3.9264729,  3.9364026,  3.9465107,
-        3.9535347], dtype='double')
+    3.6123999,  3.6163599,  3.6264927,  3.6363862,  3.646468,
+    3.6564937,  3.6664478,  3.6765388,  3.6865413,  3.6964585,
+    3.7065142,  3.716509,  3.7264658,  3.7364102,  3.7463682,
+    3.7563652,  3.7664226,  3.7763396,  3.7863384,  3.7964207,
+    3.8063589,  3.8163606,  3.8264089,  3.8364836,  3.8463381,
+    3.8563975,  3.8664163,  3.8763755,  3.8864797,  3.8964978,
+    3.9064275,  3.9164873,  3.9264729,  3.9364026,  3.9465107,
+    3.9535347], dtype='double')
 
 TEST_RSR['20']['det-1']['response'] = np.array([
-        0.01   ,  0.0118 ,  0.01987,  0.03226,  0.05028,  0.0849 ,
-        0.16645,  0.33792,  0.59106,  0.81815,  0.96077,  0.92855,
-        0.86008,  0.8661 ,  0.87697,  0.85412,  0.88922,  0.9541 ,
-        0.95687,  0.91037,  0.91058,  0.94256,  0.94719,  0.94808,
-        1.     ,  0.92676,  0.67429,  0.44715,  0.27762,  0.14852,
-        0.07141,  0.04151,  0.02925,  0.02085,  0.01414,  0.01 ], dtype='double')
+    0.01,  0.0118,  0.01987,  0.03226,  0.05028,  0.0849,
+    0.16645,  0.33792,  0.59106,  0.81815,  0.96077,  0.92855,
+    0.86008,  0.8661,  0.87697,  0.85412,  0.88922,  0.9541,
+    0.95687,  0.91037,  0.91058,  0.94256,  0.94719,  0.94808,
+    1.,  0.92676,  0.67429,  0.44715,  0.27762,  0.14852,
+    0.07141,  0.04151,  0.02925,  0.02085,  0.01414,  0.01], dtype='double')
+
+TEST_RSR_WN = {'20': {}}
+TEST_RSR_WN['20']['det-1'] = {}
+WVN = np.array([2529.38208008,  2533.8840332,  2540.390625,  2546.81494141,
+                2553.30883789,  2559.88354492,  2566.40722656,  2573.02270508,
+                2579.72949219,  2586.37451172,  2593.09375,  2599.87548828,
+                2606.55371094,  2613.41674805,  2620.29760742,  2627.18286133,
+                2634.06005859,  2641.07421875,  2648.06713867,  2655.03930664,
+                2662.14794922,  2669.25170898,  2676.36572266,  2683.50805664,
+                2690.69702148,  2697.95288086,  2705.29199219,  2712.56982422,
+                2719.94970703,  2727.43554688,  2734.8605957,  2742.38012695,
+                2749.9831543,  2757.48510742,  2765.21142578,  2768.24291992],
+               dtype='float32')
+RESP = np.array([0.01,  0.01414,  0.02085,  0.02925,  0.04151,
+                 0.07141,  0.14851999,  0.27761999,  0.44714999,  0.67429,
+                 0.92676002,  1.,  0.94808,  0.94718999,  0.94256002,
+                 0.91057998,  0.91036999,  0.95687002,  0.95410001,  0.88922,
+                 0.85412002,  0.87696999,  0.86610001,  0.86008,  0.92855,
+                 0.96077001,  0.81814998,  0.59105998,  0.33792001,  0.16644999,
+                 0.0849,  0.05028,  0.03226,  0.01987,  0.0118,
+                 0.01], dtype='float32')
+
+TEST_RSR_WN['20']['det-1']['wavenumber'] = WVN
+TEST_RSR_WN['20']['det-1']['response'] = RESP
+
 
 class ProductionClass(Calculator):
+
     def get_rsr(self):
         self.rsr = TEST_RSR
         self._wave_unit = '1e-6 m'
         self._wave_si_scale = 1e-6
-        
+
+
+class ProductionClassWN(Calculator):
+
+    """Wrapper for wavenumber calculations..."""
+
+    def __init__(self, platform, satnum, instrument, bandname,
+                 solar_flux=None, **options):
+        self.wavespace = 'wavenumber'
+        self.get_rsr()
+        super(ProductionClassWN, self).__init__(platform, satnum, instrument,
+                                                bandname, solar_flux=None,
+                                                **options)
+
+    def get_rsr(self):
+        self.rsr = TEST_RSR_WN
+        self._wave_unit = 'cm-1'
+        self._wave_si_scale = 100.
+
 
 class TestReflectance(unittest.TestCase):
+
     """Unit testing the reflectance calculations"""
-           
+
     def setUp(self):
         """Set up"""
+
+    def test_rsr_integral(self):
+        """Test calculating the integral of the relative spectral response
+        function. 
+        """
+
+        refl37 = ProductionClass('eos', '2', 'modis', '20')
+        expected = 0.18563451
+        self.assertAlmostEqual(refl37.rsr_integral, expected)
+
+        refl37 = ProductionClassWN(
+            'eos', '2', 'modis', '20', wavespace='wavenumber')
+        expected = 130.00391
+        self.assertAlmostEqual(refl37.rsr_integral, expected, 5)
 
     def test_reflectance(self):
         """Test the derivation of the reflective part of a 3.7 micron band"""
 
         refl37 = ProductionClass('eos', '2', 'modis', '20')
-        
+
         SUNZ = 80.
         TB3 = 290.
         TB4 = 282.
         REFL = refl37.reflectance_from_tbs(SUNZ, TB3, TB4)
         self.assertAlmostEqual(REFL, 0.251245010648)
 
+        SUNZ = 80.
+        TB3 = 295.
+        TB4 = 282.
+        REFL = refl37.reflectance_from_tbs(SUNZ, TB3, TB4)
+        self.assertAlmostEqual(REFL, 0.452497961)
+
+        SUNZ = 50.
+        TB3 = 300.
+        TB4 = 285.
+        REFL = refl37.reflectance_from_tbs(SUNZ, TB3, TB4)
+        self.assertAlmostEqual(REFL, 0.1189217)
 
     def tearDown(self):
         """Clean up"""
@@ -83,5 +153,5 @@ def suite():
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(TestReflectance))
-    
+
     return mysuite