12f8edeafea2f9d384fbc082df7292c8e724e821
Adam Dybbroe <adam.dybbroe@smhi.se>
2014-06-19 15:42:07 +0200

    Fixed copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -19,7 +20,9 @@ import sys, os
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../pyspectral'))
 
+
 class Mock(object):
+
     def __init__(self, *args, **kwargs):
         pass
 
@@ -38,20 +41,20 @@ class Mock(object):
             return Mock()
 
 
-MOCK_MODULES = ['numpy','scipy','xlrd',]
+MOCK_MODULES = ['numpy', 'scipy', 'xlrd', ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
 
-# -- General configuration -----------------------------------------------------
+# -- General configuration -----------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.doctest',
-              'sphinx.ext.todo','sphinx.ext.coverage',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
+              'sphinx.ext.todo', 'sphinx.ext.coverage',
               'sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -68,7 +71,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pyspectral'
-copyright = u'2013, Adam Dybbroe'
+copyright = u'2013-2014, Adam Dybbroe'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -114,7 +117,7 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 
-# -- Options for HTML output ---------------------------------------------------
+# -- Options for HTML output ---------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -194,7 +197,7 @@ html_static_path = ['_static']
 htmlhelp_basename = 'Pyspectraldoc'
 
 
-# -- Options for LaTeX output --------------------------------------------------
+# -- Options for LaTeX output --------------------------------------------
 
 # The paper size ('letter' or 'a4').
 #latex_paper_size = 'letter'
@@ -205,8 +208,8 @@ htmlhelp_basename = 'Pyspectraldoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Pyspectral.tex', u'Pyspectral Documentation',
-   u'Adam Dybbroe', 'manual'),
+    ('index', 'Pyspectral.tex', u'Pyspectral Documentation',
+     u'Adam Dybbroe', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -233,7 +236,7 @@ latex_documents = [
 #latex_domain_indices = True
 
 
-# -- Options for manual page output --------------------------------------------
+# -- Options for manual page output --------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
