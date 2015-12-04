cdb0a0b274d89f8c13d2761236e988849c74697a
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2015-12-04 15:24:01 +0100

    Add derivation of the emissive part of the 3.x signal
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- pyspectral/near_infrared_reflectance.py
+++ pyspectral/near_infrared_reflectance.py
@@ -91,10 +91,12 @@ class Calculator(RadTbConverter):
             self._get_solarflux()
         else:
             self.solar_flux = solar_flux
-        self._rad37 = None
-        self._rad37_t11 = None
+        self._rad3x = None
+        self._rad3x_t11 = None
         self._solar_radiance = None
-        self._rad39_correction = 1.0
+        self._rad3x_correction = 1.0
+        self._r3x = None
+        self._e3x = None
 
         if 'detector' in kwargs:
             self.detector = kwargs['detector']
@@ -137,7 +139,7 @@ class Calculator(RadTbConverter):
                                  "the Rosenfeld equation is supported!")
 
         LOG.debug("Derive the 3.9 micron radiance CO2 correction coefficent")
-        self._rad39_correction = (bt11 - 0.25 * (bt11 - bt13)) ** 4 / bt11 ** 4
+        self._rad3x_correction = (bt11 - 0.25 * (bt11 - bt13)) ** 4 / bt11 ** 4
 
     def _get_solarflux(self):
         """Derive the in-band solar flux from rsr over the Near IR band (3.7
@@ -149,6 +151,19 @@ class Calculator(RadTbConverter):
                                     wavespace=self.wavespace)
         self.solar_flux = \
             solar_spectrum.inband_solarflux(self.rsr[self.bandname])
+
+    def emissive_part_3x(self):
+        """Get the emissive part of the 3.x band"""
+
+        try:
+            # Emissive part:
+            self._e3x = self._rad3x_t11 * (1 - self._r3x)
+        except TypeError:
+            LOG.warning(
+                "Couldn't derive the emissive part \n" +
+                "Please derive the relfectance prior to requesting the emissive part")
+
+        return self._e3x
 
     def reflectance_from_tbs(self, sun_zenith, tb_near_ir, tb_thermal,
                              tb_ir_co2=None):
@@ -195,13 +210,13 @@ class Calculator(RadTbConverter):
                 tbco2 = np.array(tb_ir_co2)
 
         if self.instrument == 'seviri':
-            ch37name = 'IR3.9'
+            ch3xname = 'IR3.9'
         elif self.instrument == 'modis':
-            ch37name = '20'
+            ch3xname = '20'
         elif self.instrument in ["avhrr", "avhrr3", "avhrr/3"]:
-            ch37name = 'ch3b'
+            ch3xname = 'ch3b'
         elif self.instrument == 'ahi':
-            ch37name = 'ch7'
+            ch3xname = 'ch7'
         else:
             raise NotImplementedError('Not yet support for this '
                                       'instrument %s' % str(self.instrument))
@@ -209,20 +224,20 @@ class Calculator(RadTbConverter):
         if not self.rsr:
             raise NotImplementedError("Reflectance calculations without "
                                       "rsr not yet supported!")
-            # retv = self.tb2radiance_simple(tb_therm, ch37name)
+            # retv = self.tb2radiance_simple(tb_therm, ch3xname)
             # print("tb2radiance_simple conversion: " + str(retv))
             # thermal_emiss_one = retv['radiance']
-            # retv = self.tb2radiance_simple(tb_nir, ch37name)
+            # retv = self.tb2radiance_simple(tb_nir, ch3xname)
             # print("tb2radiance_simple conversion: " + str(retv))
             # l_nir = retv['radiance']
         else:
             # Assume rsr in in microns!!!
             # FIXME!
             scale = self.rsr_integral * 1e-6
-            retv = self.tb2radiance(tb_therm, ch37name, self.lut)
+            retv = self.tb2radiance(tb_therm, ch3xname, self.lut)
             # print("tb2radiance conversion: " + str(retv))
             thermal_emiss_one = retv['radiance'] * scale
-            retv = self.tb2radiance(tb_nir, ch37name, self.lut)
+            retv = self.tb2radiance(tb_nir, ch3xname, self.lut)
             # print("tb2radiance conversion: " + str(retv))
             l_nir = retv['radiance'] * scale
 
@@ -237,8 +252,8 @@ class Calculator(RadTbConverter):
 
         mu0 = np.cos(np.deg2rad(sunz))
         # mu0 = np.where(np.less(mu0, 0.1), 0.1, mu0)
-        self._rad37 = l_nir
-        self._rad37_t11 = thermal_emiss_one
+        self._rad3x = l_nir
+        self._rad3x_t11 = thermal_emiss_one
         self._solar_radiance = 1. / np.pi * self.solar_flux * mu0
 
         # CO2 correction to the 3.9 radiance, only if tbs of a co2 band around
@@ -246,26 +261,26 @@ class Calculator(RadTbConverter):
         if co2corr:
             self.derive_rad39_corr(tb_therm, tbco2)
         else:
-            self._rad39_correction = 1.0
+            self._rad3x_correction = 1.0
 
         # mask = thermal_emiss_one > l_nir
 
-        nomin = l_nir - thermal_emiss_one * self._rad39_correction
+        nomin = l_nir - self._rad3x_t11 * self._rad3x_correction
         LOG.debug("Shapes: %s  %s", str(mu0.shape),
-                  str(thermal_emiss_one.shape))
+                  str(self._rad3x_t11.shape))
         denom = self._solar_radiance - \
-            thermal_emiss_one * self._rad39_correction
+            self._rad3x_t11 * self._rad3x_correction
 
-        r39 = nomin / denom
-        # r39 = np.ma.masked_array(r39, mask=mask)
-        # r39 = np.ma.masked_where(r39 < 0, r39)
+        r3x = nomin / denom
+        # r3x = np.ma.masked_array(r3x, mask=mask)
+        # r3x = np.ma.masked_where(r3x < 0, r3x)
         # Do some further masking, also with sun-zenith:
-        # r39 = np.ma.masked_outside(r39, 0.0, 10.0)  # * 100.  # Percent!
+        # r3x = np.ma.masked_outside(r3x, 0.0, 10.0)  # * 100.  # Percent!
         # if np.ma.is_masked(tb_nir):
-        #    r39 = np.ma.masked_where(tb_nir.mask, r39).filled(0)
-        r39 = np.ma.masked_where(sunzmask, r39)
+        #    r3x = np.ma.masked_where(tb_nir.mask, r3x).filled(0)
+        self._r3x = np.ma.masked_where(sunzmask, r3x)
 
         # Reflectances should be between 0 and 1, but values above 1 is
         # perfectly possible and okay! (Multiply by 100 to get reflectances
         # in percent)
-        return r39
+        return self._r3x
