4e1c2bea5db4311433746b8a4f246f852349a65e
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-04-15 00:03:04 +0200

    remove scipy from req-file.
    RTD doesn't like it!
    
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- doc/conf.py
+++ doc/conf.py
@@ -53,6 +53,12 @@ MOCK_MODULES = ['numpy', 'numpy.core',
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#sys.path.insert(0, '/opt/lib/python2.5/site-packages')
+sys.path.insert(0, os.path.abspath('../pyspectral'))
+
 
 # -- General configuration -----------------------------------------------
 
