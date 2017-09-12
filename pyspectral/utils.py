d51b8484cd53f6619843188fd8f3bf42fffc8c02
Adam.Dybbroe <adam.dybbroe@smhi.se>
2017-09-12 12:07:23 +0200

    Add debug_on function (copy from satpy)
    Signed-off-by: Adam.Dybbroe <adam.dybbroe@smhi.se>

--- pyspectral/utils.py
+++ pyspectral/utils.py
@@ -24,6 +24,7 @@
 """Utility functions"""
 
 import os
+import logging
 from os.path import expanduser
 
 import numpy as np
@@ -312,3 +313,47 @@ def download_luts(**kwargs):
         tar.extractall(subdirname)
         tar.close()
         os.remove(filename)
+
+
+def debug_on():
+    """Turn debugging logging on.
+    """
+    logging_on(logging.DEBUG)
+
+_is_logging_on = False
+
+
+def logging_on(level=logging.WARNING):
+    """Turn logging on.
+    """
+    global _is_logging_on
+
+    if not _is_logging_on:
+        console = logging.StreamHandler()
+        console.setFormatter(logging.Formatter("[%(levelname)s: %(asctime)s :"
+                                               " %(name)s] %(message)s",
+                                               '%Y-%m-%d %H:%M:%S'))
+        console.setLevel(level)
+        logging.getLogger('').addHandler(console)
+        _is_logging_on = True
+
+    log = logging.getLogger('')
+    log.setLevel(level)
+    for h in log.handlers:
+        h.setLevel(level)
+
+
+def logging_off():
+    """Turn logging off.
+    """
+    logging.getLogger('').handlers = [NullHandler()]
+
+
+def get_logger(name):
+    """Return logger with null handle
+    """
+
+    log = logging.getLogger(name)
+    if not log.handlers:
+        log.addHandler(NullHandler())
+    return log
