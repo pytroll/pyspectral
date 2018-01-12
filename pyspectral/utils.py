79db33969af372ac679bf22c7683b85c769b392f
Adam.Dybbroe <adam.dybbroe@smhi.se>
2018-01-13 00:20:04 +0100

    Bugfix OLCI S3A. Add channel 21, rename bands and fix doc pages
    Signed-off-by: Adam.Dybbroe <adam.dybbroe@smhi.se>

--- pyspectral/utils.py
+++ pyspectral/utils.py
@@ -140,7 +140,7 @@ INSTRUMENTS = {'NOAA-19': 'avhrr/3',
                }
 
 
-HTTP_PYSPECTRAL_RSR = "https://zenodo.org/record/1146748/files/pyspectral_rsr_data.tgz"
+HTTP_PYSPECTRAL_RSR = "https://zenodo.org/record/1146757/files/pyspectral_rsr_data.tgz"
 
 AEROSOL_TYPES = ['antarctic_aerosol', 'continental_average_aerosol',
                  'continental_clean_aerosol', 'continental_polluted_aerosol',
