efe1cd70a9efde401e952b72e473fb542648a74c
Panu Lahtinen <pnuu+git@iki.fi>
2018-11-19 08:12:21 +0200

    Fix tests to check the only calculated value

--- pyspectral/tests/test_reflectance.py
+++ pyspectral/tests/test_reflectance.py
@@ -158,7 +158,7 @@ class TestReflectance(unittest.TestCase):
         tb3 = np.array([290.])
         tb4 = np.array([282.])
         refl = refl37.reflectance_from_tbs(sunz, tb3, tb4)
-        self.assertAlmostEqual(refl, 0.251245010648, 6)
+        self.assertAlmostEqual(refl.data[0], 0.251245010648, 6)
 
         tb3x = refl37.emissive_part_3x()
         self.assertAlmostEqual(tb3x, 276.213054, 6)
@@ -167,7 +167,7 @@ class TestReflectance(unittest.TestCase):
         tb3 = np.array([295.])
         tb4 = np.array([282.])
         refl = refl37.reflectance_from_tbs(sunz, tb3, tb4)
-        self.assertAlmostEqual(refl, 0.452497961, 6)
+        self.assertAlmostEqual(refl.data[0], 0.452497961, 6)
 
         tb3x = refl37.emissive_part_3x()
         self.assertAlmostEqual(tb3x, 270.077268, 6)
@@ -176,7 +176,7 @@ class TestReflectance(unittest.TestCase):
         tb3 = np.array([300.])
         tb4 = np.array([285.])
         refl = refl37.reflectance_from_tbs(sunz, tb3, tb4)
-        self.assertAlmostEqual(refl, 0.1189217, 6)
+        self.assertAlmostEqual(refl.data[0], 0.1189217, 6)
 
         tb3x = refl37.emissive_part_3x()
         self.assertAlmostEqual(tb3x, 282.455426, 6)
