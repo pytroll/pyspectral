3d12eea097e45b18be1159d3f7055d6b89cff475
Simon Proud <simon.proud@physics.ox.ac.uk>
2020-10-16 22:02:38 +0100

    Add script to print out min, central, max wavelength for a given platform/sensor/band/detector. Printed in a compatible format for entry into Satpy reader YAML files.

---
+++ bin/find_wavelengths_for_satpy.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020 Simon Proud
+
+# Author(s):
+
+#   simon.proud@physics.ox.ac.uk <simon.proud@physics.ox.ac.uk>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Retrieve channel wavelength ranges for a given sensor."""
+
+
+import argparse
+from pyspectral.rsr_reader import RelativeSpectralResponse
+from pyspectral.utils import get_wave_range
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Retrieve channel wavelength ranges for a given sensor.')
+    parser.add_argument("platform_name",
+                        help="The Platform name",
+                        type=str)
+    parser.add_argument("sensor",
+                        help="The sensor/instrument name",
+                        type=str)
+    parser.add_argument("-r", "--minimum_response",
+                        help=("Minimum response threshold: Defines the value "
+                              "that the RSR must exceed in order to trigger"
+                              "the minimum or maximum wavelengths."),
+                        default=0.15, type=float)
+    parser.add_argument("--bandname", '-b',
+                        help="The sensor band name", type=str)
+    parser.add_argument("--detector", '-d',
+                        help="The sensor detector", type=str)
+    args = parser.parse_args()
+
+    platform = args.platform_name
+    sensor = args.sensor
+    threshold = args.minimum_response
+
+    rsr = RelativeSpectralResponse(platform, sensor)
+    if args.bandname:
+        bands = [args.bandname]
+    else:
+        bands = rsr.band_names
+    if args.detector:
+        det = args.detector
+    else:
+        det = 'det-1'
+
+    for bname in bands:
+        wvls = get_wave_range(rsr.rsr[bname][det], 0.15)
+        print(f'name:  {bname}\n  wavelength: [{wvls[0]:5.3f}, {wvls[1]:5.3f}, {wvls[2]:5.3f}]')
+
