0b90deb46a21b7690eb3afbeedd731082f4c8ef7
Simon Proud <simon.proud@physics.ox.ac.uk>
2020-10-21 20:31:40 +0100

    Tidy up script that finds min/max wavelengths for sapty readers.

--- bin/find_wavelengths_for_satpy.py
+++ bin/find_wavelengths_for_satpy.py
@@ -22,45 +22,53 @@
 
 """Retrieve channel wavelength ranges for a given sensor."""
 
-
 import argparse
 from pyspectral.rsr_reader import RelativeSpectralResponse
 from pyspectral.utils import get_wave_range
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
+def _setup_argparse():
+    arg_parser = argparse.ArgumentParser(
         description='Retrieve channel wavelength ranges for a given sensor.')
-    parser.add_argument("platform_name",
-                        help="The Platform name",
-                        type=str)
-    parser.add_argument("sensor",
-                        help="The sensor/instrument name",
-                        type=str)
-    parser.add_argument("-r", "--minimum_response",
-                        help=("Minimum response threshold: Defines the value "
-                              "that the RSR must exceed in order to trigger"
-                              "the minimum or maximum wavelengths."),
-                        default=0.15, type=float)
-    parser.add_argument("--bandname", '-b',
-                        help="The sensor band name. Leave blank to return all bands.", type=str)
-    parser.add_argument("--detector", '-d',
-                        help="The sensor detector, if not passed will default to det-1",
-                        default='det-1', type=str)
-    args = parser.parse_args()
+    arg_parser.add_argument("platform_name",
+                            help="The Platform name",
+                            type=str)
+    arg_parser.add_argument("sensor",
+                            help="The sensor/instrument name",
+                            type=str)
+    arg_parser.add_argument("-r", "--minimum_response",
+                            help=("Minimum response threshold: Defines the value "
+                                  "that the RSR must exceed in order to trigger"
+                                  "the minimum or maximum wavelengths."),
+                            default=0.15, type=float)
+    arg_parser.add_argument("--bandname", '-b',
+                            help="The sensor band name. Leave blank to return all bands.", type=str)
+    arg_parser.add_argument("--detector", '-d',
+                            help="The sensor detector, if not passed will default to det-1",
+                            default='det-1', type=str)
+    return arg_parser
 
-    platform = args.platform_name
-    sensor = args.sensor
-    threshold = args.minimum_response
+
+def main(arguments):
+    platform = arguments.platform_name
+    sensor = arguments.sensor
+    threshold = arguments.minimum_response
 
     rsr = RelativeSpectralResponse(platform, sensor)
-    if args.bandname:
-        bands = [args.bandname]
+    if arguments.bandname:
+        bands = [arguments.bandname]
     else:
         bands = rsr.band_names
-    det = args.detector
+    det = arguments.detector
 
     for bname in bands:
-        wvls = get_wave_range(rsr.rsr[bname][det], args.minimum_response)
+        wvls = get_wave_range(rsr.rsr[bname][det], threshold)
         print(f'name:  {bname}')
         print(f'  wavelength: [{wvls[0]:5.3f}, {wvls[1]:5.3f}, {wvls[2]:5.3f}]')
+
+
+if __name__ == "__main__":
+
+    parser = _setup_argparse()
+    args = parser.parse_args()
+    main(args)
