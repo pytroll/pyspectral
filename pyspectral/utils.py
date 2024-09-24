85fa0802a688763f0434f4937d86b4d47b3defc6
Adam.Dybbroe <a000680@c22526.ad.smhi.se>
2024-09-24 15:42:53 +0200

    Updated the link to the new data set on Zenodo
    Signed-off-by: Adam.Dybbroe <a000680@c22526.ad.smhi.se>

--- pyspectral/utils.py
+++ pyspectral/utils.py
@@ -106,10 +106,11 @@ INSTRUMENT_TRANSLATION_DASH2SLASH = {'avhrr-1': 'avhrr/1',
                                      'avhrr-2': 'avhrr/2',
                                      'avhrr-3': 'avhrr/3'}
 
-HTTP_PYSPECTRAL_RSR = "https://zenodo.org/records/11110033/files/pyspectral_rsr_data.tgz"
+HTTP_PYSPECTRAL_RSR = "https://zenodo.org/records/13833977/files/pyspectral_rsr_data.tgz"
+
 
 RSR_DATA_VERSION_FILENAME = "PYSPECTRAL_RSR_VERSION"
-RSR_DATA_VERSION = "v1.3.0"
+RSR_DATA_VERSION = "v1.4.0"
 
 ATM_CORRECTION_LUT_VERSION = {}
 ATM_CORRECTION_LUT_VERSION['antarctic_aerosol'] = {'version': 'v1.0.1',
