2654f930cc3412bb4473e36705ab515e8e4652c7
David Hoese <david.hoese@ssec.wisc.edu>
2025-11-12 15:06:37 -0600

    Add fake rayleigh file creation

--- pyspectral/testing.py
+++ pyspectral/testing.py
@@ -7,6 +7,10 @@ import tempfile
 from pathlib import Path
 from unittest import mock
 
+import numpy as np
+
+from pyspectral.utils import ATMOSPHERES
+
 TMP_LUT_BASE_DIR = Path(tempfile.gettempdir()) / "pyspectral_fake_luts"
 
 
@@ -44,7 +48,40 @@ def mock_pyspectral_downloads(tmp_path: Path = TMP_LUT_BASE_DIR):
 
 
 def _fake_download(tarball_url: str, tarball_local_path: str | Path, extract_dir: str | Path) -> None:
-    print(tarball_url, tarball_local_path, extract_dir)
+    del tarball_url  # unused in this mocked version
+
+    extract_dir = Path(extract_dir)
+    extract_dir.mkdir(parents=True, exist_ok=True)
+    if "fake_rayleigh" in str(tarball_local_path):
+        for atmo_name in ATMOSPHERES:
+            rayl_fn = f"rayleigh_lut_{atmo_name}.h5"
+            rayl_path = extract_dir / rayl_fn
+            if rayl_path.exists():
+                continue
+            _create_fake_rayleigh_file(rayl_path, atmo_name)
+    else:
+        raise NotImplementedError("Only rayleigh LUT fake creation implemented at this time")
+
+
+def _create_fake_rayleigh_file(rayl_path: Path, atmo_name: str) -> None:
+    import h5py
+
+    with h5py.File(rayl_path, "w") as h:
+        h.info = "Fake pyspectral rayleigh LUT file"
+        h.create_dataset("azimuth_difference", data=np.linspace(0.0, 180.0, 19, dtype=np.float64))
+        # NOTE: Create constant correction for each wavelength
+        refl_data = np.repeat(np.repeat(np.repeat(
+            np.linspace(0.668, 0.115, 81, dtype=np.float64)[:, np.newaxis, np.newaxis, np.newaxis],
+            96, axis=1), 19, axis=2), 9, axis=3)
+        refl_var = h.create_dataset("reflectance", data=refl_data)
+        setattr(refl_var, "1-axis", "wavelength")
+        setattr(refl_var, "2-axis", "sun zenith secant")
+        setattr(refl_var, "3-axis", "azimuth difference angle")
+        setattr(refl_var, "4-axis", "satellite zenith secant")
+        refl_var.atmosphere = atmo_name
+        h.create_dataset("satellite_zenith_secant", data=np.linspace(1.0, 3.0, 9, dtype=np.float64))
+        h.create_dataset("sun_zenith_secant", data=np.linspace(1.0, 24.75, 96, dtype=np.float64))
+        h.create_dataset("wavelengths", data=np.linspace(400, 800, 81, dtype=np.float64))
 
 
 def cleanup_fake_luts():
