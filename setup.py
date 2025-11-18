a1cb8fa4c06283838945ba337ac4ac2bfdf1c8ac
David Hoese <david.hoese@ssec.wisc.edu>
2025-11-18 12:55:43 -0600

    Refactor RSR reader to be more clear about when the HDF5 file is loaded

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ description = ('Reading and manipulaing satellite sensor spectral responses and 
 with open('./README.md', 'r') as fd:
     long_description = fd.read()
 
-requires = ['numpy', 'scipy>=1.6.0', 'python-geotiepoints>=1.1.1',
+requires = ['numpy>=1.21', 'scipy>=1.6.0', 'python-geotiepoints>=1.1.1',
             'h5py>=2.5', 'requests', 'pyyaml', 'platformdirs']
 
 dask_extra = ['dask[array]']
